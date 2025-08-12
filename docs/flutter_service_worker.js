4e8bee6991a1f7a4915a6bd4bfff708c8e76cbbe
noobatgmd <jason.cw.hong@gmail.com>
2025-08-12 23:24:33 +0800

    Update docs with rebuilt Flutter web files

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "84c1f291ea28d744df003f9a7e5706fb
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "7a326c68aa9a10b25fa062f5071c1367",
+"flutter_bootstrap.js": "ef1f6c4f5a62f4cf3f7a36ae54ba31e9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
