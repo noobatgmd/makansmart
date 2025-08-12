90081fc5beeeb5af39bc277eda5b6f479d4bed58
noobatgmd <jason.cw.hong@gmail.com>
2025-08-12 23:07:53 +0800

    Update website with latest changes

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "84c1f291ea28d744df003f9a7e5706fb
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "7ab9a1999664b81f7cfeb6592cc91a00",
+"flutter_bootstrap.js": "0cefe3e9786cec249779e13ceda55fda",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
